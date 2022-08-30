7555c4e3bc912fceb94c4a4fe430e85ac648cdac
MaxMax89 <chip_pro89@mail.ru>
2022-08-30 09:20:56 +0300

    1

---
+++ functionphp.php
@@ -0,0 +1,34 @@
+<meta charset="utf-8"/>
+<?php
+
+function debug($data) {
+	echo '<pre>' . print_r($data, 1) . '</pre>';
+}
+
+$condidates = ['Ivan' => ['height' => 174],
+               'Maksim' => ['height' => 180],
+               'Nikolay' => ['height' => 189],
+               ];
+   function new_function($a) 
+  {
+      if ($a['height'] >= 180) true;
+      retern false;  
+  }
+
+   debug(condidates);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
