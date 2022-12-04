5866201a2f5e21793ae9a9c3c502b8ac1be97912
MaxMax89 <chip_pro89@mail.ru>
2022-12-04 23:03:22 +0300

    Chess board release 5.1

--- chess.board/app/chess.php
+++ chess.board/app/chess.php
@@ -1,9 +1,13 @@
 <?php
 $aplhapet = ["a","b","c","d","e","f","g","h"];
-$numbers = range(1 , 100);
-$count_fields =0;
 
+$count_fields =0;
 $count_fields += count($aplhapet);
+
+$numbers = range(1 , $count_fields);
+
+$numbers_revers = [];
+$numbers_revers = array_reverse($numbers);
 
 
 
@@ -14,7 +18,7 @@ $chess_board = "<table class=\"board\">";
         $chess_board .= "<tr>";
         
         if($row != $count_fields){
-            $chess_board .= "<td class=\"coordinates\">$numbers[$row]</td>";
+            $chess_board .= "<td class=\"coordinates\">$numbers_revers[$row]</td>";
         }
         else{
             $chess_board .= "<td class=\"coordinates\"></td>";
@@ -27,18 +31,18 @@ $chess_board = "<table class=\"board\">";
                 
                 if($row%2 != 0 and $row < $count_fields){
                 if($col%2 != 0 ){
-                    $chess_board .= "<td class=\"black\">$aplhapet[$col]$numbers[$row]</td>";
+                    $chess_board .= "<td class=\"black\">$aplhapet[$col]$numbers_revers[$row]</td>";
                 }
                 else{
-                     $chess_board .= "<td class=\"white\">$aplhapet[$col]$numbers[$row]</td>";
+                     $chess_board .= "<td class=\"white\">$aplhapet[$col]$numbers_revers[$row]</td>";
                 }
                 }
              elseif($row%2 == 0 and $row < $count_fields){
                  if($col%2 == 0){
-                    $chess_board .= "<td class=\"black\">$aplhapet[$col]$numbers[$row]</td>";
+                    $chess_board .= "<td class=\"black\">$aplhapet[$col]$numbers_revers[$row]</td>";
                 }
                 else{
-                    $chess_board .= "<td class=\"white\">$aplhapet[$col]$numbers[$row]</td>";
+                    $chess_board .= "<td class=\"white\">$aplhapet[$col]$numbers_revers[$row]</td>";
                     }
                 }
                    
