9ce3f05f3743bb288f420fa02b852d4c01e6850d
MaxMax89 <chip_pro89@mail.ru>
2022-12-04 22:45:20 +0300

    Chess board release 5.0

--- chess.board/app/chess.php
+++ chess.board/app/chess.php
@@ -1,51 +1,53 @@
 <?php
-$aplhapet = [false,"a","b","c","d","e","f","g","h"];
-$numbers = ["8","7","6","5","4","3","2","1"];
+$aplhapet = ["a","b","c","d","e","f","g","h"];
+$numbers = range(1 , 100);
+$count_fields =0;
 
+$count_fields += count($aplhapet);
 
 
 
 $chess_board = "<table class=\"board\">";
     
-    for ($row=0; $row <= 8; $row++) 
+    for ($row=0; $row <= $count_fields; $row++) 
     { 
         $chess_board .= "<tr>";
-        for ($col=0; $col <= 8; $col++) 
-        { 
-            if($row == 8)
-            {
-                if($col == 0){
-                    $chess_board .= "<td class=\"coordinates\"></td>";
+        
+        if($row != $count_fields){
+            $chess_board .= "<td class=\"coordinates\">$numbers[$row]</td>";
+        }
+        else{
+            $chess_board .= "<td class=\"coordinates\"></td>";
+        }
+            for ($col=0; $col < $count_fields; $col++) 
+            { 
+                if($row == $count_fields){
+                    $chess_board .= "<td class=\"coordinates\">$aplhapet[$col]</td>";
+                }
+                
+                if($row%2 != 0 and $row < $count_fields){
+                if($col%2 != 0 ){
+                    $chess_board .= "<td class=\"black\">$aplhapet[$col]$numbers[$row]</td>";
                 }
                 else{
-                    $chess_board .= "<td class=\"coordinates\">$aplhapet[$col]$numbers[$row]</td>";
+                     $chess_board .= "<td class=\"white\">$aplhapet[$col]$numbers[$row]</td>";
                 }
-            }
-            else{
-                if(($row >= 0 and $row < 8) and ($col == 0)){
-                   $chess_board .= "<td class=\"coordinates\">$numbers[$row]</td>";
                 }
-                elseif($row%2 != 0){
-                    if($col%2 != 0){
-                        $chess_board .= "<td class=\"black\">$aplhapet[$col]$numbers[$row]</td>";
-                    }
-                    else{
-                         $chess_board .= "<td class=\"white\">$aplhapet[$col]$numbers[$row]</td>";
+             elseif($row%2 == 0 and $row < $count_fields){
+                 if($col%2 == 0){
+                    $chess_board .= "<td class=\"black\">$aplhapet[$col]$numbers[$row]</td>";
+                }
+                else{
+                    $chess_board .= "<td class=\"white\">$aplhapet[$col]$numbers[$row]</td>";
                     }
                 }
-                 elseif($row%2 == 0){
-                     if($col%2 == 0){
-                         $chess_board .= "<td class=\"black\">$aplhapet[$col]$numbers[$row]</td>";
-                    }
-                     else{
-                         $chess_board .= "<td class=\"white\">$aplhapet[$col]$numbers[$row]</td>";
-                    }
-                }
-            }
+                   
         }
-        $chess_board .= "</tr>";
     }
+    
+     
 $chess_board .= "</table>";
+
 
 echo $chess_board;
 
