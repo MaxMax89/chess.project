e845ddd286f50f8105689fd92cdbc89f0886a46e
MaxMax89 <chip_pro89@mail.ru>
2022-12-09 01:35:19 +0300

    Chess board release 6.0

--- chess.board/app/chess.php
+++ chess.board/app/chess.php
@@ -1,59 +1,125 @@
 <?php
-$aplhapet = ["a","b","c","d","e","f","g","h"];
 
-$count_fields =0;
-$count_fields += count($aplhapet);
+$symbols = range('A','Z');
 
-$numbers = range(1 , $count_fields);
+$countRow = 8;
+$countCol = 8;
 
-$numbers_revers = [];
-$numbers_revers = array_reverse($numbers);
+$userColorOne = "green";
+$userColorTwo = "blue";
 
+$symbolBoard = getSymbols($symbols, $col);
 
+$tableBoard = "<table class =\"board\">";
 
-$chess_board = "<table class=\"board\">";
+$tableBoard .= getTrSymbols($symbols, $countCol);
+
+for ($row = 1 ; $row <= $countRow ; $row++) { 
+    $tableBoard .= "<tr>";
+
+    $numbersBoard = getNumbers($countRow);
+    $tdNumbers = getTdNumbers($numbersBoard, $row);
     
-    for ($row=0; $row <= $count_fields; $row++) 
-    { 
-        $chess_board .= "<tr>";
+    $tableBoard .= $tdNumbers;
+
+    for ($col = 1; $col <= $countCol ; $col++) {
         
-        if($row != $count_fields){
-            $chess_board .= "<td class=\"coordinates\">$numbers_revers[$row]</td>";
-        }
-        else{
-            $chess_board .= "<td class=\"coordinates\"></td>";
-        }
-            for ($col=0; $col < $count_fields; $col++) 
-            { 
-                if($row == $count_fields){
-                    $chess_board .= "<td class=\"coordinates\">$aplhapet[$col]</td>";
-                }
-                
-                if($row%2 != 0 and $row < $count_fields){
-                if($col%2 != 0 ){
-                    $chess_board .= "<td class=\"black\">$aplhapet[$col]$numbers_revers[$row]</td>";
-                }
-                else{
-                     $chess_board .= "<td class=\"white\">$aplhapet[$col]$numbers_revers[$row]</td>";
-                }
-                }
-             elseif($row%2 == 0 and $row < $count_fields){
-                 if($col%2 == 0){
-                    $chess_board .= "<td class=\"black\">$aplhapet[$col]$numbers_revers[$row]</td>";
-                }
-                else{
-                    $chess_board .= "<td class=\"white\">$aplhapet[$col]$numbers_revers[$row]</td>";
-                    }
-                }
-                   
+        $symbolBoard = getSymbols($symbols, $col);
+        $color = getColor($userColorOne, $userColorTwo, $row, $col);
+        $tdBoard = getTdBoard($color, $symbolBoard, $numbersBoard, $row);
+        
+        
+        $tableBoard .= $tdBoard;
+           
+    }
+    $tableBoard .= "</tr>";
+}
+
+$tableBoard .= "</table>";
+
+echo $tableBoard;
+
+
+
+
+
+
+function getTrSymbols($symbols, $countCol){
+    $trSymbols  = "<tr>";
+    $trSymbols .= "<td class=\"coordinates\"></td>";
+    for ($x=0; $x < $countCol; $x++) { 
+        $trSymbols .= "<td class=\"coordinates\">".$symbols[$x]."</td>";
+    }
+    $trSymbols .= "</tr>";
+    return $trSymbols;
+}
+
+function getTdNumbers($numbersBoard, $row){
+    $tdNumbers = "<td class=\"coordinates\">".$numbersBoard[$row - 1]."</td>";
+    return $tdNumbers;
+}
+
+function getTdBoard($color, $symbolBoard, $numbersBoard, $row){
+    $tdBoard = "<td style=\"background-color:".$color." \" class=\"white\">";
+    $tdBoard .= $symbolBoard.$numbersBoard[$row - 1]."</td>";
+    return $tdBoard;
+}
+
+function getColor($userColorOne, $userColorTwo, $row, $col){
+    $color = $userColorOne;
+    if($row % 2 != 0){
+        if($col % 2 != 0){
+            $color = $userColorTwo;
         }
     }
+    else{
+        if($col % 2 == 0){
+            $color = $userColorTwo;
+        }
+    }
+    return $color;
+}
+
+
+function getNumbers($countRow){
+    $numbers = [];
+    for ($i = $countRow; $i > 0 ; $i--) { 
+    $numbers[] = $i;   
+    }
+    return $numbers;
+}
+
+function getSymbols($symbols, $col){
+    $symbolBoard = $symbols[$col - 1];
+    return $symbolBoard;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
     
-     
-$chess_board .= "</table>";
+    
 
 
-echo $chess_board;
+
+
+    
+
+           
+
+
+
+
+
 
 
 ?>
