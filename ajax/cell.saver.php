eb2cf3b601813539177137a88481c1c677d774bd
MaxMax89 <68055296+MaxMax89@users.noreply.github.com>
2023-07-21 00:14:13 +0300

    add  function (save to log.txt only black cell)

--- ajax/cell.saver.php
+++ ajax/cell.saver.php
@@ -1,7 +1,32 @@
 <?php
 $info = $_GET["info"];
 file_put_contents("log.txt", $info, FILE_APPEND);
+$logTxt = str_split(file_get_contents("log.txt"), 2);
+
+$logInfoTxt = getLogInfo($logTxt);
+
+file_put_contents("log.txt", $logInfoTxt);
 
 
+
+
+
+
+
+
+function getLogInfo($logTxt){
+	$elCounts = array_count_values($logTxt);
+	$logInfoTxt = [];
+
+	for ($i = 0; $i < count($logTxt); $i++){
+		foreach ($elCounts as $key => $value){
+			if($logTxt[$i] == $key && $value == 1){
+				$logInfoTxt[] = $logTxt[$i];
+				break;
+			}
+		}
+	}
+	return $logInfoTxt;
+}
 
 
