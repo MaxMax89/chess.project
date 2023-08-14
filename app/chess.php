3afce4f3f5ca6a933d027031c4b79847fbf8d1d9
MaxMax89 <68055296+MaxMax89@users.noreply.github.com>
2023-08-14 20:59:58 +0300

    add branch fix

--- app/chess.php
+++ app/chess.php
@@ -1,88 +1,44 @@
 <?php
 
-$symbols = range('A', 'Z');
+$alphapet = range('A', 'Z');
 
-
-$countRow = 8;
 $countCol = 8;
+$countRow = 8;
 
-$blackCell = explode(";", file_get_contents("ajax/log.txt"));
+$symbols = range("A", $alphapet[$countCol - 1]);
+$numbers = range($countRow, 1);
+
+echo getChessBoard($symbols, $numbers);
 
 
-$tableBoard = "<table class =\"board\">";
 
-$tableBoard .= getTrSymbols($symbols, $countCol);
 
-for ($row = 1; $row <= $countRow; $row++) {
-	$tableBoard .= "<tr>";
 
-	$numbersBoard = getNumbers($countRow);
-	$tdNumbers = getTdNumbers($numbersBoard, $row);
 
-	$tableBoard .= $tdNumbers;
 
-	for ($col = 1; $col <= $countCol; $col++) {
-		$symbolBoard = getSymbols($symbols, $col);
-		$dataCell = $symbolBoard . $numbersBoard[$row - 1];
-		$tdBoard = getTdBoard($symbolBoard, $numbersBoard, $row, $dataCell, $blackCell);
 
-		$tableBoard .= $tdBoard;
 
+
+
+function getChessBoard($symbols, $numbers){
+
+	$board = "<table class='board'>";
+
+	foreach ($numbers as $number) {
+
+		$board .= "<tr>";
+		$board .= "<td class='coordinates'>$number</td>";
+		foreach ($symbols as $symbol) {
+
+			$board .= "<td class='cell_board'>" . $symbol . $number . "</td>";
+		}
+		$board .= "<td class='coordinates'>$number</td>";
+		$board .= "</tr>";
 	}
-	$tableBoard .= $tdNumbers;
-	$tableBoard .= "</tr>";
+
+	$board .= "</table>";
+	return $board;
 }
-$tableBoard .= getTrSymbols($symbols, $countCol);
-$tableBoard .= "</table>";
-
-echo $tableBoard;
-
-function getTdBoard($symbolBoard, $numbersBoard, $row, $dataCell, $blackCell)
-{
-	if (in_array($dataCell, $blackCell)) {
-		$tdBoard = "<td data-cell=" . $dataCell . " class='white black'>";
-		$tdBoard .= $symbolBoard . $numbersBoard[$row - 1] . "</td>";
-	} else {
-		$tdBoard = "<td data-cell=" . $dataCell . " class='white'>";
-		$tdBoard .= $symbolBoard . $numbersBoard[$row - 1] . "</td>";
-	}
-	return $tdBoard;
-}
-
-
-function getTrSymbols($symbols, $countCol)
-{
-	$trSymbols = "<tr>";
-	$trSymbols .= "<td class=\"coordinates\"></td>";
-	for ($x = 0; $x < $countCol; $x++) {
-		$trSymbols .= "<td class=\"coordinates\">" . $symbols[$x] . "</td>";
-	}
-	$trSymbols .= "</tr>";
-	return $trSymbols;
-}
-
-function getTdNumbers($numbersBoard, $row)
-{
-	$tdNumbers = "<td class=\"coordinates\">" . $numbersBoard[$row - 1] . "</td>";
-	return $tdNumbers;
-}
-
-
-function getNumbers($countRow)
-{
-	$numbers = [];
-	for ($i = $countRow; $i > 0; $i--) {
-		$numbers[] = $i;
-	}
-	return $numbers;
-}
-
-function getSymbols($symbols, $col)
-{
-	$symbolBoard = $symbols[$col - 1];
-	return $symbolBoard;
-}
-
 
 ?>
 
